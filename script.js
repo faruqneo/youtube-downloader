326a169e04ec5287f2771c24daa8604ba109336b
Faruq Khan <faruq@topsinfosolutions.com>
2020-02-27 12:06:34 +0530

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+var convertBtn = document.querySelector('.convert-button');
+//var convertBtn = document.querySelector("body > button");
+var URLinput = document.querySelector('.URL-input');
+
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+
+function sendURL(URL) {
+    fetch(`http://localhost:4000/download?URL=${URL}`, {
+        method: 'GET'
+    }).then(res => res.json())
+        .then(json => console.log(json));
+}
